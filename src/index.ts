9b8be8b33d5763b9cf7cb2da2a0d219b9091e86e
Dave Harms <dave.harms@fortyau.com>
2024-08-26 06:06:53 -0500

    Add logging and utility dependencies
    This commit introduces `morgan`, `winston`, `winston-daily-rotate-file`, and several associated utilities such as `fecha`, `colors`, and `logform`. These additions enhance logging capabilities within the application, enabling better monitoring and debugging. New packages such as `basic-auth` and `simple-swizzle` are also included to support logging features.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import path from 'path';
 import userRoutes from './routes/userRoutes';
 import postsRoutes from './routes/postsRoutes';
+import morgan from 'morgan';
+import logger from './logger';
 
 const envFilePath = path.resolve(
   process.cwd(),
@@ -14,6 +16,11 @@ dotenv.config({ path: envFilePath });
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(
+  morgan('combined', {
+    stream: { write: (message) => logger.info(message.trim()) },
+  }),
+);
 app.use(express.json());
 
 app.use('/users', userRoutes);
---
+++ src/logger.ts
@@ -0,0 +1,24 @@
+import winston, { createLogger, format, transports } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+const logger = createLogger({
+  level: 'info',
+  format: winston.format.combine(format.timestamp(), format.json()),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: 'logs/error-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      level: 'error',
+      maxFiles: '14d',
+    }),
+    new DailyRotateFile({
+      filename: 'logs/combined-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      level: 'info',
+      maxFiles: '14d',
+    }),
+  ],
+});
+
+export default logger;
